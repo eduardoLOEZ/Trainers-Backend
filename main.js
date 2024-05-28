c1a78c866b116755e1d15a4165a934fec4a584a4
Eduardo Loeza <84247208+eduardoLOEZ@users.noreply.github.com>
2024-05-28 08:40:03 -0600

    main comentado y actualizado

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ import router from "./routes/trainers.js";
 const app = express();
 const port = process.env.PORT || 3001;
 
+// Middleware para analizar el cuerpo de las solicitudes y manejar CORS
 app.use(express.json());
 app.use(cors());
 
@@ -17,7 +18,10 @@ app.get("/", (req, res) => {
   });
 });
 
+// Conectar a la base de datos
 connectDB();
+
+// Rutas para la API de entrenadores
 app.use("/api/trainers", router);
 
 app.listen(port, () => {
