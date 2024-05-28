70750c4b757c127d8418cddb54194054d7d23ff7
Eduardo Loeza <84247208+eduardoLOEZ@users.noreply.github.com>
2024-05-28 08:40:30 -0600

    rutas comentadas y actualizadas

--- routes/trainers.js
+++ routes/trainers.js
@@ -10,11 +10,22 @@ import {
 
 const router = express.Router();
 
+// Ruta para obtener todos los entrenadores
 router.get("/", getAllTrainers);
+
+// Ruta para obtener un entrenador por ID
 router.get("/:id", getTrainerById);
+
+// Ruta para crear un nuevo entrenador
 router.post("/", createTrainer);
+
+// Ruta para actualizar un entrenador por ID
 router.put("/:id", updateTrainer);
+
+// Ruta para eliminar un entrenador por ID
 router.delete("/:id", deleteTrainer);
+
+// Ruta para descargar la lista de entrenadores en formato CSV
 router.get("/download/csv", downloadCSV);
 
 export default router;
